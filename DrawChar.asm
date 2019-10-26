728a4296cf3f24e55108e733ca744cfa9852987b
thiccums <102100517@student.swin.edu.au>
2019-10-26 19:27:45 +1100

    updating starting position

--- DrawChar.asm
+++ DrawChar.asm
@@ -8,3 +8,14 @@
     subs r4,1 ; Decrement Character Row Counter
     bne DrawChar ; IF (Character Row Counter != 0) ;DrawChar
 bx lr
+
+DrawChars:
+  mov r4,CHAR_Y ; R4 = Character Row Counter
+  ldrb r5,[r2],1 ; R5 = Next Text Character
+  add r5,r1,r5,lsl 6 ; Add Shift To Correct Position In Font (* 64)
+  bl DrawChar
+  subs r3,1 ; Subtract Number Of Text Characters To Print
+  subne r0,SCREEN_X * CHAR_Y ; Jump To Top Of Char
+  addne r0,CHAR_X ; Jump Forward 1 Char
+  bne DrawChars ; IF (Number Of Text Characters != 0) Continue To Print Characters
+ b cont
